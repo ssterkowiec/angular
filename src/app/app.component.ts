ef002ce32d3a554657913abd8b2a93df9b568d97
ssterkowiec <Asteroida1!>
2020-10-04 13:08:41 +0200

    zad19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,50 +11,40 @@ import {UserData} from './model/UserData';
 
 })
 export class AppComponent implements OnInit {
-  http: HttpClient;
-  @ViewChild('tdForm', {static: false})
-  tdForm: any;
-  bottomMovies: any;
-  modelForm: any;
-  object: UserData = new UserData();
+  signupForm: FormGroup;
 
-  constructor(httpClient: HttpClient) {
-    this.http = httpClient;
+  constructor(private http: HttpClient) {
+    // dodajac slowko kluczowe private automatycznie tworzy sie pole http
   }
 
   ngOnInit(): void {
-    this.modelForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      age: new FormControl(),
-      country: new FormControl()
+    this.signupForm = new FormGroup({
+      id: new FormControl(),
+      title: new FormControl(),
+      userId: new FormControl(),
+      body: new FormControl()
     });
-    this.updateOnServer();
+
 
   }
 
   onSubmit() {
-    console.log(this.tdForm.value);
+    console.log(this.signupForm.value);
   }
 
-  getFromServer() {
-    this.http.get('http://jsonplaceholder.typicode.com/posts/1').subscribe(value => {
-      this.object = value as UserData;
-      console.log(value);
-      // wyciaganie danych ze strony
-    });
-  }
-
-  updateOnServer() {
-    const httpHeader = {
-      headers: new HttpHeaders({'Content-type': 'application/json ; charset=UTF-8'})
-    };
-    const body: UserData = {title: 'foo', body: 'bar', userId: 1, id: 1} as UserData;
-    this.http.put('http://jsonplaceholder.typicode.com/posts/1', body, httpHeader )
-      .subscribe(response => {
-          this.object = response as UserData;
-          console.log(response);
+  loadValues() {
+    this.http.get('http://jsonplaceholder.typicode.com/posts/1')
+      .subscribe(
+        (data: { id: number, userId: number, title: string, body: string }) => {
+          this.signupForm.controls.id.patchValue(data.id);
+          this.signupForm.controls.userId.patchValue(data.userId);
+          this.signupForm.controls.title.patchValue(data.title);
+          this.signupForm.controls.body.patchValue(data.body);
         }
       );
+
   }
+
+
 }
+
