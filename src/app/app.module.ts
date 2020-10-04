aa9d5057a0bb998a2c9add51e84938484131271d
ssterkowiec <Asteroida1!>
2020-10-04 13:30:49 +0200

    zad20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,63 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { MyDirectiveDirective } from './my-directive.directive';
-import { MyPipePipe } from './my-pipe.pipe';
+import {AppComponent} from './app.component';
+import {MyDirectiveDirective} from './my-directive.directive';
+import {MyPipePipe} from './my-pipe.pipe';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {NewsComponent} from './news/news.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRotes: Routes = [
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {page: 'Home page'},
+  },
+  {
+    path: 'news/newsId',
+    component: NewsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MyDirectiveDirective,
-    MyPipePipe
+    MyPipePipe,
+    HomeComponent,
+    LoginComponent,
+    NewsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRotes) /*wywolujemy metode i przekazujemy tablice*/
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
